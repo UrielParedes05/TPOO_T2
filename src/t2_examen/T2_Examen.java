851ae5988a09de0c59afd9145cdf5fb54ad17fc0
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-10-06 13:50:57 -0500

    Add files via upload

---
+++ src/t2_examen/T2_Examen.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t2_examen;
+
+/**
+ *
+ * @author HP
+ */
+public class T2_Examen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrameCursos frameCursos = new FrameCursos();
+        frameCursos.setLocationRelativeTo(null);
+        frameCursos.setVisible(true);
+    }
+    
+}
---
+++ src/t2_examen/entidades/Estudiante.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t2_examen.entidades;
+
+/**
+ *
+ * @author HP
+ */
+public class Estudiante {
+    private String codigo;
+    private String nombre;
+
+    public Estudiante(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
